5d39f213559034b0f2d5150ff2a131c858d7286d
Lior <lior2512@gmail.com>
2026-01-03 23:12:16 +0200

    build for prod for github pages

---
+++ docs/browser/chunk-4WZFC4Q5.js
@@ -0,0 +1,3 @@
+import{Ga as e,cb as a,fa as r,ta as t,ua as n}from"./chunk-GOWISIYT.js";var p=class i{static \u0275fac=function(o){return new(o||i)};static \u0275cmp=r({type:i,selectors:[["app-not-found"]],decls:8,vars:0,consts:[[2,"text-align","center","padding","50px"],["routerLink","/providers"]],template:function(o,d){o&1&&(t(0,"div",0)(1,"h1"),e(2,"404 - Page Not Found"),n(),t(3,"p"),e(4,"The clinic page you are looking for does not exist."),n(),t(5,"a",1),e(6,"Return to Providers List"),n()(),e(7,`
+\`
+`))},dependencies:[a],encapsulation:2})};export{p as NotFoundComponent};
